cc3d8c149201a1fa2a875a28146042714336328d
hugh fd jackson <hughfdjackson@googlemail.com>
2013-02-03 22:47:26 +0000

    added renamed files

---
+++ src/hamt.js
@@ -0,0 +1,92 @@
+var u = require('./util'),
+    hash = require('string-hash')
+
+var mask = function(hash, shift){
+    return (hash >>> shift) & 0x01f
+}
+
+var splitPositions = [0, 5, 10, 15, 20, 25, 30]
+
+// get the path from an already hashed key
+// int -> [int]
+var hashPath = function(h){
+    return splitPositions.map(mask.bind(null, h))
+}
+
+// get the path from a key
+// string -> [int]
+var keyPath = function(k){ return hashPath(hash(k)) }
+
+var reduceWhile = function(a, fn, pred, seed){
+    for ( var i = 0; i < a.length; i += 1 ) {
+        if ( ! pred(a[i], i, a) ) return seed
+        seed = fn(seed, a[i], i, a)
+    }
+    return seed
+}
+
+// a HAMT trie is one in which each non-leaf node has 32 children, from 0-32).
+// The children could be A) an empty node (represented here as an absence of
+// a property at all in the children hash), B) a trie, or C) a value object, which
+// contains the key and val (since the trie is traversed by a hash, this is needed to
+// ensure that the key is the same on retrieval.
+// Since the trie's depth is bounded by the number of 5-bit groups in the 32-bit int, when
+// collision occurs, there can only be a maximum of 7 depths of specificity.  After this, a regular 'hash'
+// value will be used as a catch all conflict resolver.
+
+// nodes
+var trie     = function(){     return { type : 'trie', children: {} } }
+var value    = function(k, v){ return { type: 'value', key: k, value: v } }
+var hashmap  = function(){     return { type: 'hashmap', values: {} } }
+
+var isTrie    = function(v){ return v.type === 'trie' }
+var isValue   = function(v){ return v.type === 'value' }
+var isHashmap = function(v){ return v.type === 'hashmap' }
+
+var unpack = function(v, pathPart, k) {
+    if ( isTrie(v)  ) return v.children[pathPart]
+}
+
+var getPathState = function(trie, k){
+    var path = keyPath(k)
+
+    // always O(n) at the moment
+    return path.reduce(function(state, pathPart){
+        var newNode = unpack(state.currNode, pathPart)
+        if ( newNode === undefined ) return state
+        return {
+            currNode: newNode,
+            realPath: state.realPath.concat([pathPart])
+        }
+    }, { currNode: trie, realPath: [] })
+}
+
+var realPath = function(trie, k){ return getPathState(trie, k).realPath }
+var realValue = function(trie, k){ return getPathState(trie, k).currNode }
+
+// immutable trie operations
+var has    = function(trie, key) {
+    // get longest possible path to the key
+    var node = realValue(trie, key)
+
+    if ( isValue(node) && node.key === key )     return true
+    if ( isHashmap(node) && key in node.values ) return true
+    else                                         return false
+}
+
+var get    = function(trie, k) {}
+var set    = function(trie, k, v) {}
+var remove = function(trie, k) {}
+
+module.exports = {
+    hashPath: hashPath,
+    keyPath : keyPath,
+    mask    : mask,
+    hash    : hash,
+    has     : has,
+    get     : get,
+    set     : set,
+    remove  : remove,
+    unpack  : unpack,
+    realPath: realPath
+}
---
+++ test/hamt-test.js
@@ -0,0 +1,77 @@
+var a = require('assert'),
+    h = require('../src/hamt')
+
+
+suite('hamt')
+
+test('t.mask splits up a 32 bit string', function(){
+    var bits = parseInt('00111110000011111000001111100000', 2)
+
+    // should chunk bits in groups of 5
+    a.equal(h.mask(bits, 0).toString(2), '0')
+    a.equal(h.mask(bits, 5).toString(2), '11111')
+})
+
+test('h.hashPath', function(){
+    var bits    = parseInt('00111110000011111000001111100001', 2),
+        path    = h.hashPath(bits),
+        strPath = path.map(function(v){ return v.toString(2) })
+
+    a.deepEqual(strPath, ['1', '11111', '0', '11111', '0', '11111', '0'], 'hashPath splits into groups of 5 bits where possible, reversing for natural traversal')
+})
+
+test('h.keyPath', function(){
+    a.deepEqual(h.keyPath('foo'), h.hashPath(h.hash('foo')), 'keyPath is hashPath . hash')
+})
+
+test('h.unpack', function(){
+    var trie = { type: 'trie',
+                 children: {
+                     3: { type: 'value', key: 'foo', value: 3 }}}
+
+    var path = h.keyPath('foo')
+    a.deepEqual(h.unpack(trie, path[0], 'foo'), { type: 'value', key: 'foo', value: 3 }, 'fetches node')
+
+    var path = h.keyPath('bar')
+    a.deepEqual(h.unpack(trie, path[0], 'bar'), undefined, 'returns undefined if there\'s nothing to unpack')
+})
+
+test('h.realPath', function(){
+    var trie = { type: 'trie',
+                 children: {
+                     3: { type: 'trie',
+                          children: {
+                              3: { type: 'value', key: 'foo', value: 4 }}}}}
+
+    a.deepEqual(h.realPath(trie, 'foo'), [3, 3], 'it should return all the parts needed to actually traverse the path up a trie')
+})
+
+test('h.has', function(){
+    var trie = { type: 'trie',
+                 children: {
+                     3: { type: 'trie',
+                          children: {
+                              3: { type: 'value', key: 'foo', value: 4 }}}}}
+
+    a.equal(h.has(trie, 'foo'), true)
+    a.equal(h.has(trie, 'bar'), false)
+
+    // on the 7-deep with the hash ending
+    var trie = { type: 'trie',
+                 children: {
+                     3: { type: 'trie',
+                          children: {
+                              3: { type: 'trie',
+                                   children: {
+                                       23: { type: 'trie',
+                                             children: {
+                                                 14: { type: 'trie',
+                                                     children: {
+                                                         24: { type: 'trie',
+                                                               children: {
+                                                                   5: { type: 'trie',
+                                                                        children: {
+                                                                            0: { type: 'hashmap', values: { 'foo': 3 }}}}}}}}}}}}}}}}
+    a.equal(h.has(trie, 'foo'), true)
+    a.equal(h.has(trie, 'bar'), false)
+})
