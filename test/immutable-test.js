4f4880549139c935763ab1915d53205c6fb9efde
hugh fd jackson <hughfdjackson@googlemail.com>
2013-05-05 00:50:14 +0100

    Added test files to repo

---
+++ test/immutable-test.js
@@ -0,0 +1,9 @@
+var a = require('assert')
+var im = require('..')
+
+describe('immutable', function(){
+	it('should export API', function(){
+		a.equal(im.object, require('../src/object'))
+		a.equal(im.array, require('../src/array'))
+	})
+})
